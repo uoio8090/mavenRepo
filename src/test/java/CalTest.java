6c014b82b24595039f8ce4b27bcbad2c50623b8a
inho.choi <inho.choi@mincoding.co.kr>
2026-03-10 17:04:35 +0900

    asd

--- src/test/java/CalTest.java
+++ src/test/java/CalTest.java
@@ -5,7 +5,14 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalTest {
 
     @Test
-    void basicUnitTest() {
+    void basicUnitTest1() {
+        Cal cal = new Cal();
+        int ret = cal.getSum(10, 20);
+        assertEquals(30, ret);
+    }
+    
+    @Test
+    void basicUnitTest2() {
         Cal cal = new Cal();
         int ret = cal.getSum(10, 20);
         assertEquals(30, ret);
