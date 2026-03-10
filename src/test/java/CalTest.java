248a5c66c5f60ac59957bf5f7262f8e9edbec986
inho.choi <inho.choi@mincoding.co.kr>
2026-03-10 17:47:24 +0900

    sdfdr3

--- src/test/java/CalTest.java
+++ src/test/java/CalTest.java
@@ -22,6 +22,6 @@ class CalTest {
     void basicUnitTest3() {
         Cal cal = new Cal();
         int ret = cal.getSum(210, 20);
-        assertEquals(230, ret);
+        assertEquals(231, ret);
     }
 }
